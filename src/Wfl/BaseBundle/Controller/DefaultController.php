db92186e3d4a7a50ef2d2f7ffb6f864aeef28eb8
fernanda <fernanda@Fferz>
2013-06-28 10:50:57 -0300

    subo proyecto + template

---
+++ src/Wfl/BaseBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wfl\BaseBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('WflBaseBundle:Default:index.html.twig' );
+    }
+}
+ //array('name' => $name)
---
+++ src/Wfl/BaseBundle/WflBaseBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wfl\BaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WflBaseBundle extends Bundle
+{
+}
